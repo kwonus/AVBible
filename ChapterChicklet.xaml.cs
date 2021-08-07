a88cf4cbd302ad2d0f0f50814a74c6709cabb93d
Kevin Wonus <sales@wonus.com>
2021-08-07 00:11:58 -0400

    Repaired incorrect book label on chapter chiclet.

--- ChapterChicklet.xaml.cs
+++ ChapterChicklet.xaml.cs
@@ -18,10 +18,12 @@ namespace AVWord.Wpf
 
         public ChapterChicklet(byte book, byte chapter, byte weight, bool green)
         {
+            InitializeComponent();
+
+            string label = ChapterChicklet.GetBookAbbreviation(book);
+            this.Book.Content = label;
             this.BookChapter = (UInt16) ((book * 0x100) + chapter);
             this.Weight = weight;
-
-            InitializeComponent();
 
             this.Show(book, chapter, green);
         }
@@ -78,6 +80,79 @@ namespace AVWord.Wpf
             this.Book.Foreground = new SolidColorBrush(Colors.Cyan);
             this.Refresh(true);
             ChapterChicklet.App.AddPanel(this);
+        }
+        static public string GetBookAbbreviation(uint num)
+        {
+            switch (num)
+            {
+                case 1: return "Gen";
+                case 2: return "Ex";
+                case 3: return "Lev";
+                case 4: return "Num";
+                case 5: return "Deut";
+                case 6: return "Josh";
+                case 7: return "Judg";
+                case 8: return "Ruth";
+                case 9: return "1Sam";
+                case 10: return "2Sam";
+                case 11: return "1Kin";
+                case 12: return "2Kin";
+                case 13: return "1Chr";
+                case 14: return "2Chr";
+                case 15: return "Ezra";
+                case 16: return "Neh";
+                case 17: return "Est";
+                case 18: return "Job";
+                case 19: return "Ps";
+                case 20: return "Prov";
+                case 21: return "Eccl";
+                case 22: return "SoS";
+                case 23: return "Is";
+                case 24: return "Jer";
+                case 25: return "Lam";
+                case 26: return "Ezek";
+                case 27: return "Dan";
+                case 28: return "Hos";
+                case 29: return "Joel";
+                case 30: return "Amos";
+                case 31: return "Obad";
+                case 32: return "Jon";
+                case 33: return "Mic";
+                case 34: return "Nah";
+                case 35: return "Hab";
+                case 36: return "Zeph";
+                case 37: return "Hag";
+                case 38: return "Zech";
+                case 39: return "Mal";
+                case 40: return "Matt";
+                case 41: return "Mark";
+                case 42: return "Luke";
+                case 43: return "John";
+                case 44: return "Acts";
+                case 45: return "Rom";
+                case 46: return "1Cor";
+                case 47: return "2Cor";
+                case 48: return "Gal";
+                case 49: return "Eph";
+                case 50: return "Phil";
+                case 51: return "Col";
+                case 52: return "1Thes";
+                case 53: return "2Thes";
+                case 54: return "1Tim";
+                case 55: return "2Tim";
+                case 56: return "Titus";
+                case 57: return "Phlm";
+                case 58: return "Heb";
+                case 59: return "Jas";
+                case 60: return "1Pet";
+                case 61: return "2Pet";
+                case 62: return "1John";
+                case 63: return "2John";
+                case 64: return "3John";
+                case 65: return "Jude";
+                case 66: return "Rev";
+            }
+            return "X";
         }
     }
 }
