b1b2fd544b0acd39f7122c1205e6f28182565320
Kevin Wonus <kevin@wonus.com>
2025-01-27 23:17:30 -0500

    Fix double Local in AddData path

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows.Markup;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("9.25.1.23")]
-[assembly: AssemblyFileVersion("9.25.1.23")]
+[assembly: AssemblyVersion("9.25.1.27")]
+[assembly: AssemblyFileVersion("9.25.1.27")]
